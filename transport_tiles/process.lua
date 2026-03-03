ca132aada3f08d89b48b5e4d17f308a9730de39e
Oliver Blanthorn <freedom4cows@gmail.com>
2026-03-03 22:33:02 +0100

    WIP transport tile ramblings

---
+++ transport_tiles/process.lua
@@ -0,0 +1,109 @@
+function relation_scan_function()
+    local type = Find("type")
+    if type == "route" or type == "route_master" then
+        Accept()
+    end
+end
+
+function node_function()
+    local highway = Find("highway")
+    local railway = Find("railway")
+    local pt = Find("public_transport")
+    local amenity = Find("amenity")
+
+    if highway == "bus_stop" or railway == "station" or railway == "stop" or railway == "halt" or pt == "station" or pt == "stop_position" or amenity == "ferry_terminal" then
+        Layer("transit_stops", false)
+        Attribute("name", Find("name"))
+        Attribute("network", Find("network"))
+        Attribute("operator", Find("operator"))
+        Attribute("ref", Find("ref"))
+        
+        if railway == "station" or pt == "station" or amenity == "ferry_terminal" then
+            Attribute("stop_type", "station")
+        else
+            Attribute("stop_type", "stop")
+        end
+    end
+end
+
+function way_function()
+    local railway = Find("railway")
+    local pt = Find("public_transport")
+    local highway = Find("highway")
+
+    if railway == "abandoned" or railway == "disused" or Find("abandoned:railway") ~= "" or Find("disused:highway") ~= "" then
+        return
+    end
+
+    if pt == "platform" or highway == "platform" or railway == "platform" then
+        Layer("transit_platforms", true) 
+        Attribute("name", Find("name"))
+        Attribute("layer", Find("layer"))
+        return
+    end
+
+    if railway == "rail" or railway == "subway" or railway == "tram" or railway == "light_rail" or railway == "funicular" or railway == "narrow_gauge" then
+        Layer("transit_tracks", false)
+        Attribute("railway", railway)
+        Attribute("tunnel", Find("tunnel"))
+        Attribute("layer", Find("layer"))
+    end
+    
+    local is_transit = false
+    local route_type = ""
+    local route_network = ""
+    local route_color = ""
+    local refs_table = {}
+
+    -- todo: make these visible at different zoom levels?
+    local hierarchy = {
+        ["ferry"] = 7,
+        ["train"] = 6,
+        ["light_rail"] = 5,
+        ["subway"] = 4,
+        ["tram"] = 3,
+        ["coach"] = 2,
+        ["bus"] = 1
+    }
+    local best_rank = 0
+
+    while NextRelation() do
+        local r_type = FindInRelation("route")
+        local current_rank = hierarchy[r_type] or 0
+
+        if current_rank > 0 then
+            is_transit = true
+            
+            local ref = FindInRelation("ref")
+            if ref ~= "" then
+                refs_table[ref] = true
+            end
+
+            if current_rank > best_rank then
+                best_rank = current_rank
+                route_type = r_type
+                route_network = FindInRelation("network")
+                route_color = FindInRelation("colour")
+            end
+        end
+    end
+
+    local final_refs = {}
+    for r, _ in pairs(refs_table) do
+        table.insert(final_refs, r)
+    end
+    table.sort(final_refs) 
+    local route_ref = table.concat(final_refs, ", ")
+
+    if is_transit then
+        Layer("transit_routes", false)
+        Attribute("route", route_type)
+        Attribute("ref", route_ref)
+        Attribute("network", route_network)
+        Attribute("colour", route_color)
+        Attribute("name", Find("name"))
+    end
+end
+
+function relation_function()
+end
---
+++ transport_tiles/readme.md
@@ -0,0 +1,17 @@
+```
+paru -S osmium-tools
+wget https://download.geofabrik.de/europe/monaco-latest.osm.pbf
+
+osmium tags-filter monaco-latest.osm.pbf \
+  r/route=bus,coach,trolleybus,share_taxi,train,subway,tram,light_rail,monorail,funicular,ferry \
+  r/route_master=bus,coach,trolleybus,share_taxi,train,subway,tram,light_rail,monorail,funicular,ferry \
+  nwr/public_transport=station,stop_position,platform \
+  nwr/railway=station,tram_stop,halt \
+  nwr/highway=bus_stop \
+  nwr/amenity=bus_station,ferry_terminal \
+  w/railway=rail,subway,tram,light_rail,monorail,funicular,narrow_gauge \
+  -o transit_only.osm.pbf
+
+osmium export transit_only.osm.pbf -o monaco_transit.geojson
+tilemaker --input transit_only.osm.pbf --config config.json --process process.lua --output out.pmtiles
+```
