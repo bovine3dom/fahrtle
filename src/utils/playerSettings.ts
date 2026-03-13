5d9452349c650d87ca5f6fa5494622f810dd29a5
Oliver Blanthorn <freedom4cows@gmail.com>
2026-03-13 16:54:14 +0100

    Bikeshed settings

--- src/utils/playerSettings.ts
+++ src/utils/playerSettings.ts
@@ -10,20 +10,20 @@ type PlayerSetting = {
 };
 
 export const defaultPlayerSettings: Record<string, PlayerSetting> = {
+  baseMap: { type: 'select', value: 'Toner-like', description: 'Base map', options: ['Positron', 'Bright', 'OSM Carto', 'Toner-like', 'Liberty (3D)', 'Virtual Earth'] },
+  publicTransportLayer: { type: 'boolean', value: true, description: 'Draw OSM public transport routes' },
   name: { type: 'text', value: undefined, description: 'Callsign', hidden: true }, // hide for now until we can change while in progress
   color: { type: 'color', value: undefined, description: 'Player colour' },
-  autoZoom: { type: 'boolean', value: true, description: 'Auto zoom' },
-  autoFollow: { type: 'boolean', value: true, description: 'Auto follow' },
-  baseMap: { type: 'select', value: 'Positron', description: 'Base map', options: ['Positron', 'Bright', 'OSM Carto', 'Toner-like', 'Liberty (3D)', 'Transport', 'Transport dark', 'Virtual Earth'] },
-  publicTransportLayer: { type: 'boolean', value: true, description: 'Draw OSM public transport routes' },
-  railwaysLayer: { type: 'select', value: 'Disabled', description: 'Railways layer', options: ['Disabled', 'Infrastructure', 'Speed', 'Electrification', 'Gauge'] },
-  hillShade: { type: 'boolean', value: true, description: 'Hillshade' },
-  terrain3d: { type: 'boolean', value: false, description: '3D terrain' },
-  bathymetry: { type: 'boolean', value: true, description: 'Water depths' },
-  debug: { type: 'boolean', value: false, description: 'Show debug buttons next to departures' },
-  firstPersonFollow: { type: 'boolean', value: false, description: 'Follow camera first person' },
+  walkConfirm: { type: 'boolean', value: false, description: 'Require confirmation before walking' },
+  hidePotentialDuplicateDepartures: { type: 'boolean', value: true, description: 'Hide duplicate departures (may hide some sleepers)' },
   maxStops: { type: 'number', value: 50, description: 'Maximum stops to show', min: 0, max: 1000 },
   showWaypoints: { type: 'boolean', value: true, description: 'Always show waypoints on map' },
-  hidePotentialDuplicateDepartures: { type: 'boolean', value: true, description: 'Hide duplicate departures (may hide some sleepers)' },
-  walkConfirm: { type: 'boolean', value: false, description: 'Require confirmation before walking' },
+  autoZoom: { type: 'boolean', value: true, description: 'Auto zoom' },
+  autoFollow: { type: 'boolean', value: true, description: 'Auto follow' },
+  hillShade: { type: 'boolean', value: true, description: 'Hillshade' },
+  bathymetry: { type: 'boolean', value: true, description: 'Water depths' },
+  debug: { type: 'boolean', value: false, description: 'Show debug buttons next to departures' },
+  railwaysLayer: { type: 'select', value: 'Disabled', description: 'Railways layer (can be laggy)', options: ['Disabled', 'Infrastructure', 'Speed', 'Electrification', 'Gauge'] },
+  terrain3d: { type: 'boolean', value: false, description: '3D terrain' },
+  firstPersonFollow: { type: 'boolean', value: false, description: 'Follow camera first person' },
 }
